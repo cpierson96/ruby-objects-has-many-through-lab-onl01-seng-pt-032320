774e1b5b842dd01fe34731b8c5e26c636350c377
Claudia Pierson <c172@umbc.edu>
2020-05-11 02:10:38 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,3 +1,13 @@
 class Genre 
-  
+  attr_accesor :name 
+  @@all = [] 
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  def songs 
+    Song.select{|song| song.genre}
+  end 
+  def artists 
+  end 
 end 
