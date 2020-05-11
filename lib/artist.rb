d1f495a732addb47685fdb4f9f99a469f73dc56a
Claudia Pierson <c172@umbc.edu>
2020-05-11 02:16:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,21 +1,26 @@
-class Artist 
-  attr_accessor :name 
-  @@all = [] 
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
   def initialize(name)
-    @name = name 
-    @@all << self 
-  end 
-  def self.all 
-    @@all 
-  end 
-  
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
   def new_song(name, genre)
     Song.new(name, self, genre)
-  end 
-  def songs 
-    Song.select{|song| song.artist == self}
-  end 
-   def genres
+  end
+
+  def songs
+    Song.all.select { |song| song.artist == self }
+  end
+
+  def genres
     songs.map(&:genre)
   end
-end 
+end
