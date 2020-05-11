89f234403d83253192d1cc7c80458a7c657d5b33
Claudia Pierson <c172@umbc.edu>
2020-05-11 02:03:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ class Artist
   attr_accessor :genre, :artist
   attr_reader :name 
   @@all = [] 
-  def initialize(name, genre,)
+  def initialize(name, genre, artist)
     @name = name 
    
   end 
