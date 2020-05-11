d5b37ada5ffdf8585f4b3fafab8306d9a4d289d4
Claudia Pierson <c172@umbc.edu>
2020-05-11 02:17:39 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,4 +1,26 @@
 class Doctor
-  
-  
-end 
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+
+  def appointments
+    Appointment.all.select { |appointment| appointment.doctor == self }
+  end
+
+  def patients
+    appointments.map(&:patient)
+  end
+end
